dffdae6e4ca109b5f727565e85e3adb48c7c84f2
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-17 19:45:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
-badge_maker(name)
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
 end
 
-batch_badge_creator(names)
+def batch_badge_creator(names)
   names.collect! { |name| badge_maker(name) }
 end
 
